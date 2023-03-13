0a8950b1b3a9b430f8ee6a88f26a334852514209
Da Do <dodatdainc@gmail.com>
2023-03-13 19:19:33 +0700

    third commit

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self): 
+        return self.name
+
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   inventory = models.IntegerField() 
+
+   def __str__(self):
+      return self.title
