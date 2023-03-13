479ce03d4d8996b38124c6fb5e19079ba3c51a1d
Da Do <dodatdainc@gmail.com>
2023-03-13 21:01:35 +0700

    sixth commit

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Booking, Menu
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
